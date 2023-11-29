7037938c5988eb47ea830439f1c3bb4e5f58ea5f
wisedev <nikitindanaj@gmail.com>
2023-11-29 15:38:11 +0300

    Added: choosing text colors

--- Watermark/Program.cs
+++ Watermark/Program.cs
@@ -9,70 +9,27 @@ class Program
 {
     static void Main()
     {
-        Console.Title = "Simple watermark";
-
+        Console.Title = "Watermark";
         PrintAscii();
+        Watermark watermark = new Watermark();
 
-        Debugger.Info("Enter the text for the watermark: ");
+        Console.Write("Enter the text for the watermark: ");
         string watermarkText = Console.ReadLine();
 
-        Debugger.Info("Enter the path to the image (including the file name): ");
-        string imagePath = Console.ReadLine();
+        Console.Write("Red, White, Green, Blue, Yellow, Pink, Orange, Magenta, Black\nEnter the text color: ");
+        string textColor = Console.ReadLine().ToLower();
+        watermark.UserColor = textColor;
 
-        Debugger.Info("Enter the transparency level (from 0.0 to 1.0): ");
+        Console.Write("Enter the path to the image (including the file name): ");
+        string imagePath = Console.ReadLine();
+        Console.Write("Enter the transparency level (from 0.0 to 1.0, for example, 0.5 corresponds to 50%): ");
         float transparency = float.Parse(Console.ReadLine());
 
-        AddWatermark(imagePath, watermarkText, transparency);
+        watermark.AddWatermark(imagePath, watermarkText, transparency);
 
         Debugger.Info("Watermark successfully added!");
-        Thread.Sleep(2000);
-    }
-
-    static void AddWatermark(string imagePath, string watermarkText, float transparency)
-    {
-        try
-        {
-            Bitmap image = new Bitmap(imagePath);
-
-            using (Graphics graphics = Graphics.FromImage(image))
-            {
-                Font font = GetFontForImage(image, watermarkText);
-                Color color = Color.FromArgb((int)(transparency * 255), 255, 255, 255);
-
-                Brush brush = new SolidBrush(color);
-
-                float x = 0.0F;
-                float y = (image.Height - font.Size) / 2;
-
-                graphics.DrawString(watermarkText, font, brush, x, y);
-            }
-
-            Debugger.Info("Enter the output file name (without extension): ");
-            string outputFileName = Console.ReadLine();
-            image.Save(outputFileName + ".png", ImageFormat.Png);
-        }
-        catch (Exception ex)
-        {
-            Debugger.Error($"Ошибка: {ex.Message}");
-        }
-    }
-
-    static Font GetFontForImage(Bitmap image, string text)
-    {
-        Font font = new Font("Arial", 1);
-
-        using (Graphics graphics = Graphics.FromImage(image))
-        {
-            SizeF textSize = graphics.MeasureString(text, font);
-
-            while (textSize.Width < image.Width && textSize.Height < image.Height)
-            {
-                font = new Font("Arial", font.Size + 1);
-                textSize = graphics.MeasureString(text, font);
-            }
-        }
-
-        return font;
+        Console.Write("\nPress any key to exit the program...");
+        Console.ReadLine();
     }
 
     static void PrintAscii()
